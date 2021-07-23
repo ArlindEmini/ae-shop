daeb649e7d1e97393a77c0c9c1bcf31566d7da46
unknown <arlind.emini7@gmail.com>
2021-07-23 11:08:10 +0200

    update Menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HAT PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop/hats" component={HatPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
