a18340db1ae230705de5b401f26bb7de1c2723ac
unknown <arlind.emini7@gmail.com>
2021-07-27 15:06:12 +0200

    finishing signIn with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.compononent";
 import FormInput from "../form-input/form-input.component";
-import { signInWithgoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithgoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -15,13 +15,20 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({
-      email: "",
-      password: "",
-    });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = (event) => {
