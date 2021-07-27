7a426d7cf7b64fda05a8f965e35b79ee06237704
unknown <arlind.emini7@gmail.com>
2021-07-27 14:18:07 +0200

    implemented sign up and ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
@@ -20,10 +20,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+
+      this.setState({
+        currentUser: userAuth,
+      });
     });
   }
 
